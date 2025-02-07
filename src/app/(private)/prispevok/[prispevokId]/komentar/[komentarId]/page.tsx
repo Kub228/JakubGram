074dbf905631a658a385b6dde68cc3e9196c7c8c
Kub <95572194+Kub228@users.noreply.github.com>
2025-02-07 11:03:18 +0100

    bolest

--- src/app/(private)/prispevok/[prispevokId]/komentar/[komentarId]/page.tsx
+++ src/app/(private)/prispevok/[prispevokId]/komentar/[komentarId]/page.tsx
@@ -1,12 +1,24 @@
-export default async function Page({
-  params
+// src\app\prispevok\[prispevokId]\komentar\[komentarId]\page.tsx
+
+import { Container } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: 'Detail komentara | ZoskaGram'};
+
+export default async function CommentDetail({ 
+  params,
 }: {
   params: Promise<{
-    prispevokId: string;
-    komentarId: string;
-  }>
+    prispevokId: string,
+    komentarId: string
+  }>;
 }) {
   const { prispevokId, komentarId } = await params;
-  
-  // Component logic using resolved parameters
+
+  return (
+    <Container>
+      <Typography>Detail komentaru cislo: {komentarId} k prispevku {prispevokId}</Typography>
+      <Typography>prispevok cislo: {prispevokId} a komentar k nemu cislo: {komentarId}</Typography>
+    </Container>
+  );
 }
